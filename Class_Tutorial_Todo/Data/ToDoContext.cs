6e855763a0d520470265985d6aadc3898e6b1485
gazeez <gazeez.prodacct@gmail.com>
2020-05-20 23:33:03 -0600

    working solution

---
+++ Class_Tutorial_Todo/Data/ToDoContext.cs
@@ -0,0 +1,19 @@
+﻿using Class_Tutorial_Todo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Class_Tutorial_Todo.Data
+{
+    public class ToDoContext: DbContext
+    {
+        public ToDoContext (DbContextOptions<ToDoContext> options) : base( options )
+        {
+
+        }
+        public DbSet<Class_Tutorial_Todo.Models.ToDoItem> ToDoItem { get; set; }
+    }
+}
---
+++ Class_Tutorial_Todo/Models/ToDoItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Class_Tutorial_Todo.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Task Required")]
+
+        public string Task { get; set; }
+
+        public bool Completed { get; set; }
+    }
+}
